42b455defaf841472ebc42a6688274dafcb16a80
gizemocak <gizem_ocak@outlook.com>
2019-06-12 18:17:07 +0000

    add validation

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,16 +4,25 @@ class ArticlesController < ApplicationController
   end
 
   def new
+    @article = Article.new
   end
 
   def create
     @article = Article.new(params.require(:article).permit(:title, :text))
  
-    @article.save
-    redirect_to @article
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
+    end
   end
 
   def show
     @article = Article.find(params[:id])
+  end
+
+ private
+  def article_params
+    params.require(:article).permit(:title, :text)
   end
 end
